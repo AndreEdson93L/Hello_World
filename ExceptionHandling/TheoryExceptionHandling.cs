a6be627a56aa623431ad2c6644decbaaf15b01da
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:37:47 +0100

    Add files via upload

---
+++ ExceptionHandling/TheoryExceptionHandling.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class TheoryExceptionHandling
+    {
+        public static void VirtalMain()
+        {
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("You cannot divide by 0.");
+            }
+            catch (ArithmeticException ex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally
+            {
+
+            }
+        }
+        public static void VMain()
+        {
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"C:\Users\Computer\Desktop");
+                var content = streamReader.ReadToEnd();
+                throw new Exception("Hey buddy");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally
+            {
+                if (streamReader != null)
+                    streamReader.Dispose();
+            }
+        }
+        public static void VTMain()
+        {
+            try
+            {
+                using (var streamReader = new StreamReader(@"C:\Users\Computer\Desktop"))
+                {
+                    var content = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/YouTubeApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access YouTube web service
+                // Read the data
+                // Create a list of Video objects
+                throw new Exception("Some low level YouTube error");
+            }
+            catch(Exception ex)
+            {
+                throw new YouTubeException("Could not fetch the vides from YouTube.", ex);
+            }
+            return new List<Video>();
+        }
+    }
+}
