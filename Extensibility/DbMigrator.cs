74815ff60cde901f855a56e27e15109980254900
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:37:17 +0100

    Add files via upload

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        //dependency injection
+        //in the constructors we are specifying the dependencies of the DbMigrator class 
+        public DbMigrator(ILogger logger)
+        {
+            this._logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at {0}" + DateTime.Now);
+
+            //Details of migrating the database
+
+            _logger.LogInfo("Migration finished at {0}" + DateTime.Now);
+        }
+    }
+}
---
+++ Extensibility/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Extensibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbMigrator1 = new DbMigrator(new ConsoleLogger());
+            var dbMigrator2 = new DbMigrator(new FileLogger(@"C:\Users\Computer\Desktop\log.txt"));
+
+            dbMigrator1.Migrate();
+            dbMigrator2.Migrate();
+
+            /*
+             OCP Open Close Principle in OOP
+
+             Theory
+             Software entities should be open for extensions
+             and close for modifications.
+
+             Practice
+             We didn't change the DbMigrator
+             we have extended the class using the extension point*
+             
+             *extension point -> public DbMigrator(ILogger logger*){ _logger = logger }
+             */
+            Console.ReadKey();
+        }
+    }
+}
