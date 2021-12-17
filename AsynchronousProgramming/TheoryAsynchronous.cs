0584fab8b17f47659c796a8357ad8dfee885be1f
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:39:28 +0100

    Add files via upload

---
+++ AsynchronousProgramming/TheoryAsynchronous.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynchronousProgramming
+{
+    public class TheoryAsynchronous
+    {
+        //Synchronous Program Execution
+
+        /*
+         * Program is executed line by line, one at a time.
+         * When a function is called, program execution has to wait until the function returns.
+         * 
+         * 1: ...
+         * 2: TimeConsumingProcess();
+         * 3: ...
+         * 4: ...
+         */
+
+        //Asynchronous Program Execution
+
+        /*
+         * When a function is called, program execution continues to the next line,
+         * without waiting for the function to complete.
+         * 
+         * 1: ...
+         * 2: TimeConsumingProcess(callback);
+         * 3: ...
+         * 4: ...
+         * 
+         * callback()
+         * {
+         *    ...
+         *  }
+         */
+
+        //So what's the difference?
+
+        /* Asynchronous programming improves responsiveness. */
+
+        //Real-world Examples
+
+        /*
+         * Windows Media Player
+         * Web Browser
+         */
+
+        //When to use asynchronous?
+
+        /*
+         * Accessing the Web
+         * Working with files and databases
+         * Working with images
+         */
+
+        //How?
+
+        /*Traditional approaches:
+         * - Multi-threading
+         * - Callbacks
+         */
+
+        /*New approache since NET 4.5
+         * - Async / Await
+         */
+    }
+}
