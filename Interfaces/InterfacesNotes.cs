4dbacffb91cbada587c577224e271f3220564a94
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:36:36 +0100

    Add files via upload

---
+++ Interfaces/InterfacesNotes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public class InterfacesNotes
+    {
+        public void PrintNotes()
+        {
+            Dictionary<string, string> dizionario = new Dictionary<string, string>();
+
+            dizionario.Add("What?", "A languange construct that is similar to a class (in terms of syntax) but is fundamentally different");
+            dizionario.Add("Syntax", "public interface ITaxCalculator\n{\n\tint Calculate();\n}");
+            dizionario.Add("Whi?", "To build loosely-coupled applications.");
+            dizionario.Add("How?", "OrderProcessor ----->  <<interface>> ITaxCalculator.");
+
+            foreach (var item in dizionario)
+                Console.WriteLine("Key: {0}\n{1}\n", item.Key, item.Value);
+        }
+    }
+}
+    
---
+++ Interfaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var notes = new InterfacesNotes();
+            //notes.PrintNotes();
+            
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
+        }
+    }
+}
