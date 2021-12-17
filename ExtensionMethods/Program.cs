9b8cf6feeb05f625d8b2babab6780cedbc6156be
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:41:33 +0100

    Add files via upload

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is a post and it's very long but it's ok";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18};
+            var max = numbers.Max();
+
+            System.Console.WriteLine(shortenedPost);
+            System.Console.WriteLine(max);
+        }
+    }
+}
---
+++ ExtensionMethods/TheoryExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public class TheoryExtensionMethods
+    {
+        //What are Extension Methods?
+
+        /*
+         * Allow us to add methods to an existing class without
+         * - changing its source code, or
+         * - creating a new class that inherits from it
+         */
+    }
+}
