e29a82035e936c27842a5c0ee99027ba65e3047f
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:38:56 +0100

    Add files via upload

---
+++ Dynamic/DynamicTheory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamic
+{
+    public class DynamicTheory
+    {
+        //Programming languages
+
+        //Statically typed languages: C#, Java
+        //Dynamically-typed languages: Ruby, Javascript, Python
+
+        //Type resolution
+
+        //Static languages: at compile-time
+        //Dynamic languages: at run-time
+
+        //Benefits
+
+        //Static languages: early feedback (compile-time)
+        //Dynamic languages: easier and faster to code
+
+        //C# History
+
+        //Started as a static language
+        //.NET 4 added the dynamic capability, to improve interoperability with
+        //  - COM (eg writing office applications)
+        //  - Dynamic languages (IronPython)
+        public static void VirtualMain()
+        {
+            //REFLECTION
+            object obj = "Andrea Edson";
+            //obj.GetHashCode();
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+
+            //DYNAMIC
+            dynamic excelObject = "Cell1 Cell2 ...";
+            excelObject.Optimize();
+
+            //On .NET 4
+            //DILR seats on top of CILR and gives dynamic language capability to C#
+        }
+    }
+}
---
+++ Dynamic/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Dynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic name = "Andrea Edson";
+            Console.WriteLine(name);
+
+            name = 10;
+            Console.WriteLine(name);
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b; //becuase a and b are dynamic c will end up to be dynamic
+            Console.WriteLine(c);
+
+            //ConversionOrCast
+            int i = 5;
+            dynamic d = i;
+            Console.WriteLine(d.GetType());
+
+            long l = d; // l == 5 and it's long type
+        }
+    }
+}
