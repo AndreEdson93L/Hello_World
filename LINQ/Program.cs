5434b21a59061b0caca17b54260194de7a7fc0ae
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:32:58 +0100

    Add files via upload

---
+++ LINQ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Extension Methods
+            var book = books.SingleOrDefault(b => b.Title == "ASP.NET MVC");
+            Console.WriteLine(book == null);
+            Console.WriteLine(book.Title + "\n");
+
+            book = books.FirstOrDefault(b => b.Title == "C# Advanced Topics");
+            Console.WriteLine(book.Title + " " + book.Price + "\n");
+
+            book = books.LastOrDefault(b => b.Title == "C# Advanced Topics");
+            Console.WriteLine(book.Title + " " + book.Price + "\n");
+
+            var pagedBooks = books.Skip(2).Take(3);
+            foreach (var pagedBook in pagedBooks)
+                Console.WriteLine(pagedBook.Title);
+
+            var count = books.Count();
+            Console.WriteLine("\n" + count);
+
+            var maxPrice = books.Max(b => b.Price);
+            Console.WriteLine("\n" + maxPrice);
+
+            var minPrice = books.Min(b => b.Price);
+            Console.WriteLine("\n" + minPrice);
+
+            var totalPrices = books.Sum(b => b.Price);
+            Console.WriteLine("\n" + totalPrices);
+
+            //Recap
+            books.Where(b => b.Price < 8);
+            books.Single();
+            books.SingleOrDefault();
+
+            books.First();
+            books.FirstOrDefault();
+
+            books.Last();
+            books.LastOrDefault();
+
+            books.Min();
+            books.Max();
+            books.Count();
+            books.Sum(b => b.Price);
+            books.Average(b => b.Price);
+
+            books.Skip(5).Take(3); 
+        }
+    }
+}
---
+++ LINQ/TheoryLINQ.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class TheoryLINQ
+    {
+        //What is LINQ?
+
+        /*
+         * Stands for: Language Integrated Query
+         * Gives you the capability to query objects
+         */
+
+        //You can query
+        /*
+         * Objects in memory, eg collections (LINQ to Objects)
+         * Databases (LINQ to Entities)
+         * XML (LINQ to XML)
+         * ADO.NET Data Sets (LINQ to Data Sets)
+         */
+        public static void VirtualMain()
+        {
+            var books = new BookRepository().GetBooks();
+
+            // LINQ Query Operators
+            var cheaperBooks = from b in books
+                               where b.Price < 10
+                               orderby b.Title
+                               select b;
+
+            // LINQ Extension Methods
+            var cheapBooks = books
+                .Where(b => b.Price < 10) // Anonymous method (b [argument] => b.Price < 10 [boolean expression])
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            /*
+            var cheapBooks = new List<Book>();
+            foreach (var book in books)
+            {
+                if (book.Price < 10)
+                    cheapBooks.Add(book);
+            }
+            */
+
+            foreach (var book in cheapBooks)
+                Console.WriteLine(book);
+            //Console.WriteLine(book.Title + " " + book.Price);
+        }
+    }
+}
