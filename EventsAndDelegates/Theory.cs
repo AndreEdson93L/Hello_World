eb3e12c22c09456a0da35310a8ee3233154865b9
AndreEdson93L <96044703+AndreEdson93L@users.noreply.github.com>
2021-12-17 02:38:20 +0100

    Add files via upload

---
+++ EventsAndDelegates/Theory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class Theory
+    {
+        //EVENTS
+
+        /*
+         * A mechanism for communication between objects
+         * Used in building Loosely Coupled Applications
+         * Helps extending applications
+         */
+
+        //DELEGATES
+
+        /*
+         * Agreement / Contract between Publisher and Subscriber
+         * Determines the signature of the event handler method in Subscriber
+         */
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+    public class VideoEncoder
+    {
+        // 1 - Define a delegate
+        // 2 - Define an event based on that delegate
+        // 3 - Raise the event
+
+
+        //EventHandler              <- costrutto di .NET per evitare di creare un delegate ogni volta che creiamo un delegate
+        //EventHandler<TEventArgs>  <- //
+
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;
+
+        //New way to declare an event
+
+        //public event EventHandler<VideoEventArgs> VideoEconded;
+        //public event EventHandler VideoEncoding;
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding Video...");
+            Thread.Sleep(3000);
+
+            OnVideoEncoded(video);
+        }
+
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if (VideoEncoded != null)
+                VideoEncoded(this, new VideoEventArgs() { Video = video });
+        }
+    }
+}
